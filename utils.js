e34103233b999914638288e74473c5fcbf3e1c84
s-n-graham <sn.graham@outlook.com>
2022-07-20 19:49:30 +0300

    1st VSCODE commit

---
+++ utils.js
@@ -0,0 +1,11 @@
+const debounce = (callback, delay=1000) => {
+    let timeOutId;
+    return (...args) => {
+        if (timeOutId) {
+            clearTimeout(timeOutId);
+        }
+        timeOutId = setTimeout(() => {
+            callback.apply(null, args);
+        }, delay);
+    };
+};
